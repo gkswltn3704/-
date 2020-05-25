b7ddf7a826a65ac40c06e98825b3af7b976086cc
goott2 <goott2@DESKTOP-OQFDRIA>
2020-05-25 16:47:06 +0900

    spring web07 anno hw version

---
+++ spring_web07_controller_anno_hw/WebContent/WEB-INF/jsp/view.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>view.jsp</title>
+</head>
+<body>
+	<h1>view.jsp</h1>
+	<h2>data1 : ${data }</h2>
+	<h2>data2 : ${data2 }</h2>
+	
+</body>
+</html>
---
+++ spring_web07_controller_anno_hw/src/spring_web07_controller_anno_hw/ViewMenuController.java
@@ -0,0 +1,28 @@
+package spring_web07_controller_anno_hw;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ViewMenuController {
+	
+	@RequestMapping(value = "viewData1.do")
+	public ModelAndView show1(HttpServletRequest req) {
+		return new ModelAndView("view", "data", req.getParameter("message"));
+		
+	}
+	
+	@RequestMapping(value = "viewData2.do")
+	public String show2(HttpServletRequest req) {
+		
+		String data = req.getParameter("message");
+		
+		req.setAttribute("data2", data);
+		
+		return "view";
+	}
+	
+}
