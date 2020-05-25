b537e19dbcc49021fd049b80773c556efb3cdc1f
goott2 <goott2@DESKTOP-OQFDRIA>
2020-05-25 16:41:09 +0900

    spring web03 hw version

---
+++ spring_web03_hw/src/spring_web03_hw/NowController.java
@@ -0,0 +1,30 @@
+package spring_web03_hw;
+
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class NowController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		ModelAndView mav = new ModelAndView();
+
+		Date d = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd - HH:mm:ss");
+		String time = sdf.format(d);
+		
+		mav.addObject("time", time); 
+		mav.setViewName("nowTime");
+		
+		return mav;
+	}
+	
+}
